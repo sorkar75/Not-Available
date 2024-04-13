cf1c5127a1f3f0897c38ef318fa5ed4b08e79853
NOOB Romon <152087346+noobromon@users.noreply.github.com>
2024-04-13 23:05:05 +0600

    Update 404-jsr.js

--- File/file1/file/404/404-jsr.js
+++ File/file1/file/404/404-jsr.js
@@ -9,3 +9,36 @@ function redirect(url, delay) {
 redirect("https://example.com", 50); // 5000 milliseconds = 5 seconds
 */
 
+const overlayElement = document.getElementById('overlay');
+const containerElement = document.getElementById('container');
+
+// Fetching content from the first URL to populate the overlay
+fetch('https://rawcdn.githack.com/noobromon/live.notice/5208b24564e0003ce94b3bc0337f95623dd7830a/notice.txt')
+  .then(response => response.text())
+  .then(data => {
+    overlayElement.innerText = data.trim();
+  })
+  .catch(error => {
+    console.error('Error fetching data from the first URL:', error);
+    hideContainer();
+  });
+
+// Fetching content from the second URL to determine whether to show or hide the overlay
+fetch('https://cdn.jsdelivr.net/gh/noobromon/Not-Available@live/File/file1/file/404/404.txt')
+  .then(response => response.text())
+  .then(data => {
+    if (data.trim().toLowerCase() === "on") {
+      overlayElement.style.display = "block";
+    } else {
+      hideContainer();
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching data from the second URL:', error);
+    hideContainer();
+  });
+
+function hideContainer() {
+  overlayElement.style.display = "none";
+  containerElement.style.display = "none";
+}
