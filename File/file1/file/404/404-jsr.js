36fba8f36d6d615e0f70a45a4e63b3a1367e1501
NOOB Romon <152087346+noobromon@users.noreply.github.com>
2024-04-13 22:39:46 +0600

    Update 404-jsr.js

--- File/file1/file/404/404-jsr.js
+++ File/file1/file/404/404-jsr.js
@@ -1,1 +1,11 @@
+/*// Redirect to a specific URL after a specified time (in milliseconds)
+function redirect(url, delay) {
+    setTimeout(function() {
+        window.location.href = url;
+    }, delay);
+}
+
+// Example usage: Redirect to "https://example.com" after 5 seconds
+redirect("https://example.com", 50); // 5000 milliseconds = 5 seconds
+*/
 
